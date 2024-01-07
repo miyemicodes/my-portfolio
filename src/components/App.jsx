6836e737e6c82c56033b47f8538a193a4ac65af2
miyemicodes <lizzymiyemi@gmail.com>
2024-01-06 22:04:45 -0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,12 @@ import Introduction from "./introduction";
 
 function App() {
   return (
-<div className="container w-full h-screen flex align-items-center justify-items-center bg-slate-500 sm:flex flex-col lg:flex-row">
-   <Introduction/>
-   <Details/>
+<div className="container w-full h-full flex align-items-center justify-items-center bg-slate-800">
+  <div className=" sm:flex flex-col my-9 mx-5 lg:flex-row">
+    <Introduction/>
+    <Details/>
+    </div>
+  
 </div>
 
   );
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,7 +4,45 @@ function Details(){
  
 return ( 
   <div className="port-details">
-     <h1 className="text-3xl font-bold underline">Alright</h1>
+
+  <div className="py-32">
+    <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">ABOUT</h2>
+    <p className="text-lg text-slate-400">
+    A skilled Software Developer with experience in the industry.
+    While my expertise is primarily in frontend development, I have
+    a track record of delivering cutting-edge systems for small and
+    intermediate enterprises including fintech, education ,personal
+    brands and portfolio. My proficiency extends to projects with a
+    creative ,responsive and performance-driven approach.
+
+    <br/>
+
+    When I'm not coding ,I do love to read novels , watch Movies , 
+    Listen to music , sleeping and playing Games.
+    </p>
+  </div>
+
+  <div>
+  <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">SKILLS</h2>
+
+
+  </div>
+
+  <div>
+  <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">EXPERIENCE</h2>
+
+  </div>
+
+  <div>
+  <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">EDUCATION</h2>
+
+  </div>
+
+  <div>
+  <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">PROJECTS</h2>
+
+  </div>
+
   </div>
 
 );
