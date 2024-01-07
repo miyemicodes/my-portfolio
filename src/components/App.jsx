da8c9a98b7f5d4567ad607f6e4034bc6576b60e7
miyemicodes <lizzymiyemi@gmail.com>
2024-01-06 20:13:31 -0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Details from "./Details";
 import Introduction from "./introduction";
 
+
 function App() {
   return (
-<div className="container">
-
+<div className="container w-full h-screen flex align-items-center justify-items-center bg-slate-500 sm:flex flex-col lg:flex-row">
    <Introduction/>
    <Details/>
 </div>
