dd3379084e6f715e4830abcfca6d6b1c04f21d9e
miyemicodes <lizzymiyemi@gmail.com>
2024-01-08 07:39:00 -0800

    initial commit

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -42,15 +42,21 @@ return (
         Jan 2023- Present
       </div>
       <div className="experience-details">
-      <h2>Freelancer <MdOutlineArrowOutward /></h2>
-      <p>Fiverr </p>
-      <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti hic vero
-      dignissimos numquam, ducimus saepe odit iste omnis cumque quo voluptates
-      libero laborum asperiores. Eaque, voluptas similique. Eum, similique
-      consequatur.
-      </p>
-        </div>
+          <h2>Freelancer <MdOutlineArrowOutward /></h2>
+          <p>Fiverr </p>
+          <p>
+          Over the course of my career, I have worked as a Front End Web
+          Developer, showcasing expertise in transforming diverse design 
+          formats like PSD, XD, Sketch, and FIGMA into HTML, CSS, Tailwind CSS
+            and Bootstrap. I possess a strong command of HTML5, CSS3, Bootstrap, 
+            Tailwind CSS, JavaScript, and React JS. I am proud of my proficiency
+            in developing responsive web templates, mobile templates, User 
+            friendly websites and engaging in front-end development using a 
+            variety of front-end technologies.
+          </p>
+
+          <a href="https://www.fiverr.com/s/qzV2DZ">Contact me on Fiverr <MdOutlineArrowOutward /></a>
+      </div>
     </div>
   </div>
 );
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,7 +5,9 @@ function Skills(){
     <div>
     <h2 className="py-10 text-base font-bold tracking-wide text-slate-600 ">SKILLS</h2>
   
-  
+    <div className="skills-list">
+      <p>ReactJS</p>
+    </div>
     </div>
   );
 }
