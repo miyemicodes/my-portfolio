83e4431e605c4ea274a3fff4629fc433ce09cecb
miyemicodes <lizzymiyemi@gmail.com>
2024-04-23 09:51:31 -0700

    more edits

--- src/components/portfolio-nav.jsx
+++ src/components/portfolio-nav.jsx
@@ -10,16 +10,28 @@ function Nav() {
         >
           About
         </a>
-        <a href="#experience" className="hover:translate-x-6 ">
+        <a
+          href="#experience"
+          className="transition ease-in-out delay-150 hover:scale-112 hover:text-slate-200 hover:translate-x-6 duration-300"
+        >
           Experience
         </a>
-        <a href="#skill" className="hover:translate-x-6 ">
+        <a
+          href="#skill"
+          className="transition ease-in-out delay-150 hover:scale-112 hover:text-slate-200 hover:translate-x-6 duration-300"
+        >
           Skill
         </a>
-        <a href="#education" className="hover:translate-x-6 ">
+        <a
+          href="#education"
+          className="transition ease-in-out delay-150 hover:scale-112 hover:text-slate-200 hover:translate-x-6 duration-300"
+        >
           Education
         </a>
-        <a href="#project" className="hover:translate-x-6 ">
+        <a
+          href="#project"
+          className="transition ease-in-out delay-150 hover:scale-112 hover:text-slate-200 hover:translate-x-6 duration-300"
+        >
           Project
         </a>
       </nav>
--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -15,25 +15,25 @@ function Socials() {
           </a>
         </li>
 
-        <li className="text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
+        <li className="text-2xl transition ease-in-out delay-180 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
           <a href="https://www.instagram.com/oritsemiyemi?igsh=OHA0djU2NDczZXZv">
             <FaInstagram />
           </a>
         </li>
 
-        <li className="text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
+        <li className="text-2xl transition ease-in-out delay-200 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
           <a href="https://x.com/Elizabe85838502?t=zXG_nKWUzPrbMBV2G0nddw&s=35">
             <FaXTwitter />
           </a>
         </li>
 
-        <li className="text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
+        <li className="text-2xl transition ease-in-out delay-220 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
           <a href="https://www.linkedin.com/in/elizabeth-godfrey-180a11199?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
             <FaLinkedin />
           </a>
         </li>
 
-        <li className="text-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
+        <li className="text-2xl transition ease-in-out delay-250 hover:-translate-y-1 hover:scale-112 hover:text-slate-200 duration-300">
           <a href="https://github.com/miyemicodes">
             <FaMediumM />
           </a>
