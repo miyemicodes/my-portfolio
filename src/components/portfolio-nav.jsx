52dfa2bf9be0f22a072499d4a35effb7ba04983c
miyemicodes <lizzymiyemi@gmail.com>
2024-04-23 09:38:47 -0700

    edits

--- src/components/portfolio-nav.jsx
+++ src/components/portfolio-nav.jsx
@@ -4,7 +4,10 @@ function Nav() {
   return (
     <div className="port-Nav hidden lg:block">
       <nav className="text-slate-300 text-xl flex flex-col gap-2">
-        <a href="#about" className="hover:translate-x-6 ">
+        <a
+          href="#about"
+          className="transition ease-in-out delay-150 hover:scale-112 hover:text-slate-200 hover:translate-x-6 duration-300"
+        >
           About
         </a>
         <a href="#experience" className="hover:translate-x-6 ">
