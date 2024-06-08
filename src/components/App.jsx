f6764c974d4b306342cc3878496e4af1906e04d6
miyemicodes <lizzymiyemi@gmail.com>
2024-06-07 18:00:18 -0700

    Adding Effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Details from "./Details";
 import Introduction from "./Introduction";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <section>
       <div className="w-screen relative md:h-screen grid grid-cols-1 md:grid-cols-2 bg-slate-800 mix-blend-difference">
+        <div
+          className="absolute w-24 h-24 bg-gradient-to-r from-white via-transparent to-transparent rounded-full pointer-events-none"
+          style={{
+            top: mousePosition.y,
+            left: mousePosition.x,
+            transform: "translate(-50%, -50%)",
+          }}
+        ></div>
         <Introduction />
         <Details />
       </div>
-
-     {/* <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-64 h-64 rounded-full pointer-events-none bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-transparent to-gray-700"></div> */}
     </section>
   );
 }
